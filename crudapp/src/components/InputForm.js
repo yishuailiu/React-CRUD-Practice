ec927b552b8a8228c168c8fcffb51497d4e289a8
yishuailiu <43007048+yishuailiu@users.noreply.github.com>
2021-03-20 19:03:36 -0600

    Update InputForm.js

--- crudapp/src/components/InputForm.js
+++ crudapp/src/components/InputForm.js
@@ -2,14 +2,14 @@ import React, { useState ,useContext} from 'react';
 import ReactDOM from 'react-dom';
 import 'antd/dist/antd.css';
 import {Form,Input,Button,Select} from 'antd';
-
+import { useHistory } from "react-router-dom";
 import {CrudAppContext} from '../store/crudAppStore';
 
 const {Option} = Select;
 
 const InputForm = (props) =>{
     const {itemList,categories,createItem} = useContext(CrudAppContext);
-
+    let history = useHistory();
     const renderCategories = (props) =>{
         return categories.map(c => {
             return <Option key={c}>{c}</Option>
@@ -20,13 +20,15 @@ const InputForm = (props) =>{
     
     const submitHandler = (value) =>{
         let newItem = {
-            id:Date.now(),
+            id:Date.now().toString(),
             desc:value.description,
             cate:value.category,
             content:value.content,
             
         };
         createItem(newItem);
+        console.log(itemList);
+        history.push('/list')
     };
 
     return <Form name='crudForm' onFinish={submitHandler}>
